c70a654a9ca5178b36c33670af020cbf0025c56e
Dylan <dylanliaa@gmail.com>
2025-10-14 23:13:58 -0400

    small change to about me

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -15,8 +15,7 @@ Engineering Physics
       `I'm Dylan, a student at the University of Michigan. Go Blue!
 Currently, I'm contributing to the Hazel project in the Future of Programming Lab.
 I'm also involved with CLAWS and Innovation for Impact, two software-related clubs.
-In my spare time, I love to work on side-projects and run with friends.
-Shameless plug, I use Vim keybinds`,
+In my spare time, I love to work on side-projects, run with friends, eat food, and MonkeyType.`,
     //"At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.jpeg",
   skills: [
