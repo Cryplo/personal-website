5fbf96f162ba5e97c606d70911bfea0e84c83753
Dylan <dylanliaa@gmail.com>
2025-10-21 10:42:41 -0400

    update photo of me

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -16,7 +16,7 @@ Currently, I'm contributing to the Hazel editor in the Future of Programming Lab
 I'm also involved with CLAWS and Innovation for Impact, two software-related clubs.
 In my spare time, I love to work on side-projects, run with friends, eat food, and MonkeyType.`,
     //"At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-  avatarUrl: "/me.jpeg",
+  avatarUrl: "/me.png",
   skills: [
     "React",
     "Next.js",
