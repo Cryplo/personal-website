942162e358a7f135809f256acb661d22590de7a5
Dylan <dylanliaa@gmail.com>
2025-12-08 22:48:08 -0500

    gravity background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import SplashScreenProvider from "@/components/splash-screen-provider";
+import NebulaBackground from "@/components/nebula-background";
 import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
-import SpaceBodyBackground from "./space";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -60,14 +60,14 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "flex bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+          "flex font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
           fontSans.variable
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
           <SplashScreenProvider>
             <TooltipProvider delayDuration={0}>
-              <SpaceBodyBackground />
+              <NebulaBackground />
               {children}
               <Navbar />
             </TooltipProvider>
